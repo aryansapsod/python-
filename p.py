1c894c23b8f38c73d56d8e7816ad9034c829dcde
Aryan Sapsod <111196181+aryansapsod@users.noreply.github.com>
2025-11-21 21:16:19 +0530

    Add files via upload

---
+++ p.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+
+def function():
+    cars={
+        'model':'hundai-i20','company':'hundai','price':5,
+        'model':'hundai-i10','company':'hundai','price':5
+
+
+    }
+    print(cars)
+    inf=pd.Series(cars)
+    print(inf)
+# function()
+
+import matplotlib.pyplot as plts
+
+ax =plts.subplots()
+plts.show()
+# print(ax)
