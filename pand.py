7f96588326efda450936353ac64bd4b15bf05199
Aryan Sapsod <sapsodaryan@gmail.com>
2025-11-22 20:49:54 +0530

    insert data

--- pand.py
+++ pand.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-# df =pd.read_csv('C:\\Users\\DELL\\\\sales_data_sample.csv',encoding="latin1")
-# print(df)
+df =pd.read_csv('C:\\Users\\DELL\\\\sales_data_sample.csv',encoding="latin1")
+print(df)
 
 df =pd.read_excel("D:\\Book1.xlsx")
 df =pd.DataFrame(df)
@@ -31,8 +31,21 @@ print('-'*60)
 Data = {
     "Name":['Aryan','Rohit','Nitin','Raj','Amar'],
     "Age":[21,23,21,25,24],
-    "City":["Latur","Pune",'Mumbai','Delhi','Murud']
+    "City":["Latur","Pune",'Mumbai','Delhi','Murud'],
+    'Name':['Ram','sham','yash','rohit','shreyash'],
+    'Age' :[20,33,40,21,25],
+    'City':['murud','dhoki','nilanga','nagar','ahilya']
 }
 df =pd.DataFrame(Data)
+print(df)
+print('-'*120)
+
+insert_data ={
+    'Name':['Ram','sham','yash','rohit','shreyash'],
+    'Age' :[20,33,40,21,25],
+    'City':['murud','dhoki','nilanga','nagar','ahilya']
+
+}
+df=pd.DataFrame(insert_data)
 print(df)
 print('-'*120)
