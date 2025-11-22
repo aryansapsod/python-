7570792ca3b0dea681a1eaac48c5104c9059ddea
Aryan Sapsod <sapsodaryan@gmail.com>
2025-11-22 17:19:55 +0530

    changes

--- pand.py
+++ pand.py
@@ -1,17 +1,25 @@
 import pandas as pd
 
-# # df =pd.read_csv('C:\\Users\\DELL\\\\sales_data_sample.csv',encoding="latin1")
-# # print(df)
+# df =pd.read_csv('C:\\Users\\DELL\\\\sales_data_sample.csv',encoding="latin1")
+# print(df)
 
-# df =pd.read_excel("D:\\Book1.xlsx")
-# print(df)
-# df= pd.read_json("c:\\Users\\DELL\\Downloads\\sample_Data.json")
-# print(df)
+df =pd.read_excel("D:\\Book1.xlsx")
+df =pd.DataFrame(df)
+print('-'*120)
+print(df)
+df= pd.read_json("c:\\Users\\DELL\\Downloads\\sample_Data.json")
+df =pd.DataFrame(df)
+print('-'*120)
+
+print(df)
 
 df = pd.read_excel("D:\\Book1.xlsx")
-# print(df)
+df =pd.DataFrame(df)
 
-# print(df.tail())
+print(df)
+print('-'*120)
+
+print(df.tail())
 Data = {
     "Name":['Aryan','Rohit','Nitin','Raj','Amar'],
     "Age":[21,23,21,25,24],
@@ -19,3 +27,12 @@ Data = {
 }
 df =pd.DataFrame(Data)
 print(df)
+print('-'*60)
+Data = {
+    "Name":['Aryan','Rohit','Nitin','Raj','Amar'],
+    "Age":[21,23,21,25,24],
+    "City":["Latur","Pune",'Mumbai','Delhi','Murud']
+}
+df =pd.DataFrame(Data)
+print(df)
+print('-'*120)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+=pd.read_csv('C:\\Users\\DELL\\\\sales_data_sample.csv',encoding="latin1")
+print(df)
