aff26a41702c4191190e3de621ae2fb8ef40b1fd
Aryan Sapsod <sapsodaryan@gmail.com>
2025-11-22 17:05:45 +0530

    adding some cars

--- p.py
+++ p.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-
+print('-'*60)
 def function():
     cars={
         'model':'hundai-i20','company':'hundai','price':5,
@@ -8,13 +8,28 @@ def function():
 
 
     }
-    print(cars)
+    # print(cars)
     inf=pd.Series(cars)
     print(inf)
-# function()
+function()
 
-import matplotlib.pyplot as plts
+# import matplotlib.pyplot as plts
 
-ax =plts.subplots()
-plts.show()
-# print(ax)
+# ax =plts.subplots()
+# plts.show()
+# # print(ax)
+print('-'*60)
+def function1():
+    cars={
+        'model':'hundai-carnious','company':'hundai','price':16,
+        'model':'hundai-maxnex','company':'hundai','price':50
+
+
+    }
+    # print(cars)
+    inf=pd.Series(cars)
+    print(inf)
+function1()
+print('-'*60)
+
+
