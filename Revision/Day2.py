5cb68c8a4275e0713d37d7ec3619f04fe7d9f6bb
Aryan Sapsod <sapsodaryan@gmail.com>
2026-01-03 16:28:20 +0530

    Day2

---
+++ Revision/Day2.py
@@ -0,0 +1,11 @@
+# in C Programming
+#       # include <stdio.h>
+#       int main() {
+#           printf("Hello, World!\n");
+#           return 0;
+#       }
+
+print("Revision Day 2: Hello World in Different Languages")
+
+# Hello World in Python
+print("Hello, World!")
