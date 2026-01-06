3a5db17b5a9354b75ab30dcee8973fd3bba8f63e
Aryan Sapsod <sapsodaryan@gmail.com>
2026-01-06 14:10:45 +0530

    Addimision Process Completed

--- Revision/Day6.py
+++ Revision/Day6.py
@@ -1,2 +1,33 @@
 #  Banking Project
 
+#  Project Using Elif And Nested If-Else Statements
+
+# Student Age
+
+#  Student College Admisssion Process
+
+age = int(input("Enter Your Age: "))
+if age>=18 and age<=25:
+    print("You Are Eligible For Addmission")
+    
+    Percentage=int(input("Enter Your Percentage: "))
+    if Percentage>=70:
+        print("You need to pay 7 Lakh ")
+        
+    elif Percentage>=80:
+        print("Your Addmission IS Successful")
+        
+    elif Percentage<60 and Percentage>70:
+        print("You Need to Pay 7 Lakh + Additional Charges")
+        
+    elif Percentage<=50:
+        print("Not Eligible Due To Inefficient Percentage")
+        
+    else:
+        print("You Are Not Eligible For Addmission!")
+        
+else:
+    print("You Are Not Eligible For Addition Due To  Age!")
+        
+
+     
