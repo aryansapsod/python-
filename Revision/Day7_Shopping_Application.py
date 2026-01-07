38a2eb8814211cb77c1e12beb70ca33da44b22b7
Aryan Sapsod <sapsodaryan@gmail.com>
2026-01-07 22:45:04 +0530

    Shopping Application

---
+++ Revision/Day7_Shopping_Application.py
@@ -0,0 +1,11 @@
+
+print(int(input('Enter Your Choice: ')))
+
+print("""   
+                !!! Welcome To Vaibhav Dryfruit Kirana And Masale !!!
+      
+      1.Menu
+      2.Payment
+      3.OrderInfo
+      
+      """)
