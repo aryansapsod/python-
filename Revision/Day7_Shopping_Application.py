7727e824f4bb33767f9cb69803fc7c699268c8d5
Aryan Sapsod <sapsodaryan@gmail.com>
2026-01-07 22:54:46 +0530

    Shopping Application

--- Revision/Day7_Shopping_Application.py
+++ Revision/Day7_Shopping_Application.py
@@ -1,5 +1,5 @@
 
-print(int(input('Enter Your Choice: ')))
+# print(int(input('Enter Your Choice: ')))1
 
 print("""   
                 !!! Welcome To Vaibhav Dryfruit Kirana And Masale !!!
@@ -9,3 +9,23 @@ print("""
       3.OrderInfo
       
       """)
+
+if int(input('Enter Your Choice:')) ==1:
+    print("""
+          Our Menu Is As Follows:
+          
+          1.Dryfruits
+          2.Kirana
+          3.Masale
+          
+          """)
+    print("For More Info Contact: 7066050740")
+    
+elif int(input('Enter Your Choice:')) ==2:
+    print("Payment Gateway Is Under Maintenance, Please Try Again Later")
+    
+elif int(input('Enter Your Choice:')) ==3:
+    print("Your Order Is Being Processed, You Will Receive It Soon")   
+    
+else:
+    print("Invalid Choice, Please Try Again")
