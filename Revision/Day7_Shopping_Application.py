ebab9763c960441d2b801d3c38cbed703ca22b25
Aryan Sapsod <sapsodaryan@gmail.com>
2026-01-10 22:04:48 +0530

    3rd product

--- Revision/Day7_Shopping_Application.py
+++ Revision/Day7_Shopping_Application.py
@@ -216,6 +216,13 @@ if int(input("Enter Your Choice: "))==3:
             quantity = int(input())
             total = quantity * 500
             print("Total Price: ", total, "INR")
+            
+        elif int(input("Enter Your Choice: "))==4:
+            print("Garam Masala Powder Price Per Kg: 600 INR")
+            print("Enter Quantity in Kgs: ")
+            quantity = int(input())
+            total = quantity * 600
+            print("Total Price: ", total, "INR")
       
 
 else:
